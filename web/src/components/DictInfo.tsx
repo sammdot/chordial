46982a3998c4c0dce415d9ce88465e24fceffbb8
Sammi De Guzman <sammi@sammdot.ca>
2022-05-19 19:45:21 -0700

    Add badges for dictionary visibility and proprietary dictionaries

--- web/src/components/DictInfo.tsx
+++ web/src/components/DictInfo.tsx
@@ -1,4 +1,4 @@
-import { FaCheck } from "react-icons/fa"
+import { FaCheck, FaDollarSign, FaEyeSlash, FaLock } from "react-icons/fa"
 
 import { Dictionary } from "src/api/models"
 import { EntryCounter } from "src/components/Counter"
@@ -10,7 +10,43 @@ type Props = {
   dict: Dictionary
 }
 
-export function DictBadge({ dict }: Props) {
+export function VisibilityBadge({ dict }: Props) {
+  return dict.visibility === "public" ? (
+    <></>
+  ) : (
+    <div
+      className={
+        "inline-block translate-y-[-0.25rem] px-1.5 pt-0.5 rounded-md text-sm font-medium text-white ml-3 select-none " +
+        (dict.visibility === "unlisted" ? "bg-amber-400" : "bg-gray-400")
+      }
+    >
+      {dict.visibility === "unlisted" ? (
+        <>
+          <FaEyeSlash className="inline translate-y-[-1px] mr-1" />
+          Unlisted
+        </>
+      ) : (
+        <>
+          <FaLock className="inline translate-y-[-1px] mr-1" />
+          Private
+        </>
+      )}
+    </div>
+  )
+}
+
+export function ProprietaryBadge({ dict }: Props) {
+  return dict.proprietary ? (
+    <div className="inline-block translate-y-[-0.25rem] px-1.5 pt-0.5 rounded-md text-sm font-medium bg-sky-400 text-white ml-3 select-none">
+      <FaDollarSign className="inline translate-y-[-1px] mr-1" />
+      Proprietary
+    </div>
+  ) : (
+    <></>
+  )
+}
+
+export function TheoryBadge({ dict }: Props) {
   return dict.theory ? (
     <div className="inline-block translate-y-[-0.25rem] px-1.5 pt-0.5 rounded-md text-sm font-medium bg-brand text-white ml-3 select-none">
       <FaCheck className="inline translate-y-[-1px] mr-1" />
@@ -30,7 +66,9 @@ export default function DictInfo({ dict }: Props) {
             <Link to={`/${dict.user.username}`}>{dict.user.username}</Link>
             <span className="mx-2">/</span>
             <span className="font-medium">{dict.name}</span>
-            <DictBadge dict={dict} />
+            <ProprietaryBadge dict={dict} />
+            <VisibilityBadge dict={dict} />
+            <TheoryBadge dict={dict} />
           </span>
         </div>
         <div className="text-md text-gray-700">
